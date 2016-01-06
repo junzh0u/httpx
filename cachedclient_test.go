f7737a1559159e912cdeb365e57e5b9241726cb2
Jun Zhou <junz@junz.info>
2016-01-06 12:12:38 -0800

    cached client test

--- cachedclient_test.go
+++ cachedclient_test.go
@@ -4,6 +4,30 @@ import (
   "testing"
 )
 
+func TestCachedClientGetDocument(t *testing.T) {
+  succ_cases := []string {
+    "http://junz.info",
+  }
+  fail_cases := []string {
+    "http://junz.info/404",
+  }
+  cc, _ := NewCachedClient("/tmp/utfhttp.cache.json")
+  defer cc.Close()
+
+  for _, url := range succ_cases {
+    _, err := cc.GetDocument(url)
+    if err != nil {
+      t.Errorf("Failed: %s", url)
+    }
+  }
+  for _, url := range fail_cases {
+    _, err := cc.GetDocument(url)
+    if err == nil {
+      t.Errorf("Should fail while not: %s", url)
+    }
+  }
+}
+
 func BenchmarkCachedClientGetSuccess(b *testing.B) {
   succ_cases := []string {
     "http://junz.info",
