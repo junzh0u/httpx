a482c21778818763e80966b7811b28963d6824e1
Jun Zhou <junz@junz.info>
2016-03-17 17:20:54 -0700

    more comments

--- utf.go
+++ utf.go
@@ -1,22 +1,23 @@
 package httpx
 
 import (
-  "io/ioutil"
-  "net/http"
+	"io/ioutil"
+	"net/http"
 
-  "golang.org/x/net/html/charset"
+	"golang.org/x/net/html/charset"
 )
 
+// RespBodyInUTF8 takes a http.Response and returns its body as UTF8 string
 func RespBodyInUTF8(resp *http.Response) (string, error) {
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    return "", err
-  }
-  contenttype := resp.Header.Get("Content-Type")
-  encoding, _, _ := charset.DetermineEncoding(body, contenttype)
-  utfbody, err := encoding.NewDecoder().Bytes(body)
-  if err != nil {
-    return "", err
-  }
-  return string(utfbody), nil
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	contenttype := resp.Header.Get("Content-Type")
+	encoding, _, _ := charset.DetermineEncoding(body, contenttype)
+	utfbody, err := encoding.NewDecoder().Bytes(body)
+	if err != nil {
+		return "", err
+	}
+	return string(utfbody), nil
 }
