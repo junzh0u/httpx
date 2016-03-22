8d87d2f55f5b0a642d2a79ce8be7013d8b88f8ca
Jun Zhou <junz@junz.info>
2016-03-22 11:19:28 -0700

    clean tmp db after testing

--- cache_test.go
+++ cache_test.go
@@ -1,18 +1,25 @@
 package httpx
 
 import (
+	"io/ioutil"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
 func TestCached(t *testing.T) {
-	db, err := leveldb.OpenFile("/tmp/httpx.test.db", nil)
-	defer db.Close()
+	tmpdir, err := ioutil.TempDir("", "httpx.test.")
 	if err != nil {
 		t.Error(err)
 	}
+	defer os.RemoveAll(tmpdir)
+	db, err := leveldb.OpenFile(tmpdir, nil)
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
 	cachedGet := Cached(db)(http.Get)
 	for i := 0; i < 10; i++ {
 		_, err := cachedGet("http://www.google.com")
@@ -23,11 +30,16 @@ func TestCached(t *testing.T) {
 }
 
 func BenchmarkCached(b *testing.B) {
-	db, err := leveldb.OpenFile("/tmp/httpx.test.db", nil)
-	defer db.Close()
+	tmpdir, err := ioutil.TempDir("", "httpx.test.")
 	if err != nil {
 		b.Error(err)
 	}
+	defer os.RemoveAll(tmpdir)
+	db, err := leveldb.OpenFile(tmpdir, nil)
+	if err != nil {
+		b.Error(err)
+	}
+	defer db.Close()
 	cachedGet := Cached(db)(http.Get)
 	for i := 0; i < b.N; i++ {
 		_, err := cachedGet("http://www.google.com")
