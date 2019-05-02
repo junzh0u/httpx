b98cd97a05d6d5c1e248b31cd1779ee5290d018a
Jun Zhou <junz@junz.info>
2019-05-02 13:36:41 -0700

    remove phantomjs dependency

--- cmd/httpx/httpx.go
+++ cmd/httpx/httpx.go
@@ -5,12 +5,11 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/benbjohnson/phantomjs"
 	"github.com/junzh0u/httpx"
 )
 
-func get(url string, getbody httpx.ReadBodyFunc) {
-	body, err := getbody(url)
+func exec(url string, readbody httpx.ReadBodyFunc) {
+	body, err := readbody(url)
 	if err != nil {
 		panic(err)
 	}
@@ -18,15 +17,12 @@ func get(url string, getbody httpx.ReadBodyFunc) {
 }
 
 func main() {
-	phantomjs.DefaultProcess.Open()
-	defer phantomjs.DefaultProcess.Close()
-
 	flag.Parse()
 
 	switch flag.Arg(0) {
 	case "insecure":
-		get(flag.Arg(1), httpx.ReadBodyInUTF8(httpx.GetInsecure))
+		exec(flag.Arg(1), httpx.ReadBodyInUTF8(httpx.GetInsecure))
 	default:
-		get(flag.Arg(0), httpx.ReadBodyInUTF8(http.Get))
+		exec(flag.Arg(0), httpx.ReadBodyInUTF8(http.Get))
 	}
 }
